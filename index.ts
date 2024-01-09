84411e44c3d4a5b4845df510ef9d4e6c2b87e49e
berenar <bernatpericasserra97@gmail.com>
2024-01-09 22:47:29 +0100

    Improve logging and naming

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import puppeteer from "puppeteer";
 const keywords = ["veter"];
 const urlBoib = "https://www.caib.es/eboibfront/ca";
 
-console.log("Starting...");
+console.log("Starting browser...");
 // Launch the browser and open a new blank page
 const browser = await puppeteer.launch({
   headless: true,
@@ -27,18 +27,21 @@ const lastBoibUrl = await (
 console.log(`Navigating to ${lastBoibUrl}`);
 await page.goto(lastBoibUrl);
 
-const section2 = await (
+const section2url = await (
   await page.waitForSelector(
     "#bs-example-navbar-collapse-2 > ul > li > ul > li:nth-child(3) > a"
   )
 )?.evaluate((element) => element.href);
 
-console.log(`Navigating to ${section2}`);
-await page.goto(section2);
+console.log(`Navigating to ${section2url}`);
+await page.goto(section2url);
 
 const allText = await page.content();
 
-const found = keywords.map((keyword) => {
+console.log("Closing browser...");
+await browser.close();
+
+const results = keywords.map((keyword) => {
   if (allText.match(keyword)) {
     console.log(`Matched ${keyword}`);
     console.log("🎉🎉🎉 Uep! Opos!");
@@ -46,9 +49,9 @@ const found = keywords.map((keyword) => {
   }
   return { keyword, found: false };
 });
-console.log(`Results:`, found);
+console.log(`Results:`, results);
 
-if (!found.some((i) => i.found)) {
+if (!results.some((i) => i.found)) {
   console.log("👎👎👎 No he trobat res...");
 }
 
